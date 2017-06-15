1c0f1d81a6894726ac7e0578b0fa956a627d7f37
dan gleyzer <dangl@post.bgu.ac.il>
2017-06-15 19:41:12 +0300

    /

--- Data.py
+++ Data.py
@@ -1,21 +1,21 @@
 import pandas as pd
 import os
 import re
-from scipy.stats import mode
+
 
 class data:
     _structureDict = {}
     _train_df = None
 
-    def __init__(self, path,bins):
-        self.bins=bins
+    def __init__(self, path, bins):
+        self.bins = bins
         self.path = path
         self.getStructure()
-      # self.printDict();
+        # self.printDict();
 
-# Add Structure to dictionary
+    # Add Structure to dictionary
     def getStructure(self):
-        lines = tuple(open(self.path+"/Structure.txt", 'r'))
+        lines = tuple(open(self.path + "/Structure.txt", 'r'))
         for line in lines:  # get line by line
             splitedLine = line.split()
             if splitedLine[2] == "NUMERIC":
@@ -31,10 +31,10 @@ class data:
             print(" ")
 
     def loadTrainDataFrame(self):
-        self._train_df = pd.read_csv(self.path+"/train.csv")
+        self._train_df = pd.read_csv(self.path + "/train.csv")
         self.fillMissingValues()
 
-    def fillMissingValues (self):
+    def fillMissingValues(self):
         print("The null valus:")
         print(self._train_df.apply(lambda x: sum(x.isnull()), axis=0))
         for columnName in self._train_df.columns:
@@ -63,7 +63,8 @@ class data:
                 for j in range(self.bins):
                     labels.append(j + 1)
                 df[column] = pd.cut(column, bins=cutpoints, labels=labels, include_lowest=True)
-####main####
-Data = data(os.path.dirname(os.path.realpath(__file__)),3)
-Data.loadTrainDataFrame("train")
 
+
+####main####
+Data = data(os.path.dirname(os.path.realpath(__file__)), 3)
+Data.loadTrainDataFrame()
